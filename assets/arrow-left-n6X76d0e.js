82af299220d8877397aa01945418b415de396321
Ben-Vincent Adamo <Ben@Adamo.de>
2026-06-14 11:46:07 +0200

    Updates

--- assets/arrow-left-n6X76d0e.js
+++ assets/arrow-left-n6X76d0e.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-Za0Eb1PJ.js";/**
+import{c as e}from"./index-CO_F3p3L.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
