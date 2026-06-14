ee3a0fbfc8e7555120e3be4036e54f2e527a884a
Ben-Vincent Adamo <Ben@Adamo.de>
2026-06-14 11:38:06 +0200

    Updates

--- assets/arrow-left-B-0olCf7.js
+++ assets/arrow-left-B-0olCf7.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BLqKYFtc.js";/**
+import{c as e}from"./index-Za0Eb1PJ.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
